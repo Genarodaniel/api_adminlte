d8ee89513493da094c325d498b9a576e6ade97cf
Genarodaniel <37519144+Genarodaniel@users.noreply.github.com>
2019-09-11 20:40:58 -0300

    update method to condominium and minor changes

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,10 @@ Route::delete('/data/{r}', 'DataController@destroy')->name('path.destroy');
 Route::get('/condominiums','CondominiumController@list');
 Route::post('/condominium','CondominiumController@store');
 route::get('/condominium/{id}','CondominiumController@show');
-route::post('update','CondominiumController@update');
+route::post('/condominium/update','CondominiumController@update');
 
-Route::prefix('users')->group(function(){ 
-	
+Route::prefix('users')->group(function(){
+
 	Route::post('login','UserController@login');
 	Route::post('login_app','User_appController@login');
 	Route::post('register','UserController@register');
@@ -33,14 +33,14 @@ Route::prefix('users')->group(function(){
 	Route::post('add_user','User_appController@store')->name('add_user');
 	Route::get('/users_api', 'UserController@all_users')->name('all_users');
 	Route::get('/users_api/{id}', 'UserController@show')->name('single_user');
-	 
+
 
 	//Route::prefix('users')->group(function(){
 	//route::get('/users','ResidentController@all_users')->name('all_users');
 	//Route::get('/ok',function(){
 		//return ['status'=>true];
 	//});
-	
-	
+
+
 
 ?>
