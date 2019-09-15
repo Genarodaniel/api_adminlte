7b18cae92903ae845038f67596700b8ce364a851
Genarodaniel <37519144+Genarodaniel@users.noreply.github.com>
2019-09-15 15:10:50 -0300

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,23 @@ Route::post('/data', 'DataController@store')->name('path.store');
 Route::get('/data/{id}', 'DataController@show')->name('path.show');
 Route::put('/data/{data}', 'DataController@update')->name('path.update');
 Route::delete('/data/{r}', 'DataController@destroy')->name('path.destroy');
+
+Route::post('add_user','User_appController@store')->name('add_user');
+Route::get('/users_api', 'UserController@all_users')->name('all_users');
+Route::get('/users_api/{id}', 'UserController@show')->name('single_user');
 //Route::get('/ok','ResidentController@all_users')->name('ok');
 
+Route::prefix('condominium')->group(function(){
 
-Route::get('/condominiums','CondominiumController@list');
-Route::post('/condominium','CondominiumController@store');
-route::get('/condominium/{id}','CondominiumController@show');
-route::post('update','CondominiumController@update');
+	Route::get('/list','CondominiumController@list');
+	Route::post('/add_condominium','CondominiumController@store');
+	route::get('/{id}','CondominiumController@show');
+	route::post('update','CondominiumController@update');
+});
 
-Route::prefix('users')->group(function(){ 
-	
+
+Route::prefix('users')->group(function(){
+
 	Route::post('login','UserController@login');
 	Route::post('login_app','User_appController@login');
 	Route::post('register','UserController@register');
@@ -30,17 +37,7 @@ Route::prefix('users')->group(function(){
 
 });
 
-	Route::post('add_user','User_appController@store')->name('add_user');
-	Route::get('/users_api', 'UserController@all_users')->name('all_users');
-	Route::get('/users_api/{id}', 'UserController@show')->name('single_user');
-	 
 
-	//Route::prefix('users')->group(function(){
-	//route::get('/users','ResidentController@all_users')->name('all_users');
-	//Route::get('/ok',function(){
-		//return ['status'=>true];
-	//});
-	
-	
+
 
 ?>
