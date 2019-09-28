cb2401034c4bf530cb784bc77afb9ef1bebd4353
Genarodaniel <37519144+Genarodaniel@users.noreply.github.com>
2019-09-28 14:46:31 -0300

    utensils route

--- app/Http/Controllers/UtensilCondController.php
+++ app/Http/Controllers/UtensilCondController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Models\UtensilCond;
 
 class UtensilCondController extends Controller
 {
     public function __construct(UtensilCond $utensilCond)
     {
         $this->utensilCond = $utensilCond;
+    }
+
+    public function exist($utensil_id)
+    {
+        $exists = DB::table('utensilsCond')->where('utensil_id','=',$utensil_id)->first();
+        return $exists;
     }
 }
