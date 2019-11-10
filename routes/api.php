d8842bc69315a43a9b44aa613b450036bf0c7498
Genarodaniel <37519144+Genarodaniel@users.noreply.github.com>
2019-11-10 15:15:54 -0300

    add call to delete methods

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ Route::prefix('api_user')->group(function(){
 	// api user routes
 	Route::post('login','UserController@login')->name('api_user.login');
 	Route::post('register','UserController@register')->name('api_user.store');
+	Route::delete('delete','UserController@delete')->name('api_user.delete');
 
 	Route::group(['middleware' =>'auth:api'], function(){
 		Route::get('details','UserController@details')->name('api_user.details');
@@ -20,6 +21,7 @@ Route::prefix('api_user')->group(function(){
 			Route::post('add','User_appController@store')->name('user.store');
 			Route::get('get/{id}', 'User_appController@show')->name('user.show');
 			Route::put('update/{id}', 'User_appController@update')->name('user.update');
+			Route::delete('delete','User_appController@delete')->name('user.delete');
 
 		});
 
@@ -28,6 +30,7 @@ Route::prefix('api_user')->group(function(){
 			Route::post('add','CondominiumController@store')->name('condominium.store');
 			route::get('get/{id}','CondominiumController@show')->name('condominium.show');
 			route::put('update/{id}','CondominiumController@update')->name('condominium.update');
+			Route::delete('delete','CondominiumController@delete')->name('condominium.delete');
 		});
 
 		Route::prefix('utensil')->group(function(){
@@ -35,16 +38,19 @@ Route::prefix('api_user')->group(function(){
 			Route::post('add','UtensilController@store')->name('utensil.store');
 			Route::put('update/{id}','UtensilController@update')->name('utensil.update');
 			Route::get('get/{id}','UtensilController@show')->name('utensil.show');
+			Route::delete('delete','UtensilController@delete')->name('utensil.delete');
 
 			Route::post('add_schedule','UtensilScheduleController@store')->name('utensilSchedule.store');
 			Route::put('update_schedule','UtensilScheduleController@update')->name('utensilSchedule.update');
-			Route::get('list_shedules/{utensil_id}','UtensilScheduleController@list')->name('UtensilShedule.list');
+			Route::get('list_shedules/{utensil_id}','UtensilScheduleController@list')->name('UtensilSchedule.list');
+			Route::delete('delete_schedule','UtensilScheduleController@delete')->name('utensilSchedule.delete');
 		});
 
 		Route::prefix('reserve')->group(function(){
 			Route::post('add','ReservController@store')->name('reserve.store');
-			Route::post('listReserv','ReservController@list');
-			Route::put('update','ReservController@update');
+			Route::post('listReserv','ReservController@list')->name('reserve.list');
+			Route::put('update','ReservController@update')->name('reserve.update');
+			Route::delete('delete','ReservController@delete')->name('reserve.delete');
 
 		});
 
